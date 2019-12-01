de3b64c1fe7fb1f82d90ea8628dcf33beae5e0ff
Marvio Rocha <marviorocha@marviorocha.com>
2019-12-01 12:08:17 -0300

    add new updates to working

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+
+/* navigation menu */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+
+/* FAQ Start */
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -14,11 +47,17 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+/* Scrool pages */
+
 function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
 
-$("#autor").click(function() {
-   scrollToAnchor('autor');
-});
+
+$("#link1").click(function() { scrollToAnchor('link1') }); 
+$("#link2").click(function() { scrollToAnchor('link2') }); 
+$("#link3").click(function() { scrollToAnchor('link3') }); 
+$("#link4").click(function() { scrollToAnchor('link4') }); 
+$("#link5").click(function() { scrollToAnchor('link5') }); 
+$("#link6").click(function() { scrollToAnchor('link6') }); 
